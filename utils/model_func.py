b862d020c6238a59f7f8208a9af5acbcfac04d3e
Dakoh Wisdom <wiizzydreadmill@gmail.com>
2023-08-02 20:31:05 +0100

    changed model to neural network

--- utils/model_func.py
+++ utils/model_func.py
@@ -1,6 +1,8 @@
 import pickle
+import os
 import tensorflow as tf
 from tensorflow import keras
+from sklearn.model_selection import train_test_split
 from typing import List
 from database.schema import Users
 from exceptions.custom_execption import BadRequestException
@@ -27,9 +29,74 @@ class FaceEncoding():
 
 
 
+def get_train_test_data(path:str):
 
-async def train_and_evaluate_model(model):
-    pass
+    
+
+    for entry in os.scandir(path):
+
+        if entry.is_dir():
+
+            class_id = entry.path.split("\\")[-1]
+
+            print(class_id)
+
+
+    # for filename in os.listdir(os.path.join(os.getcwd(), "64c5783c513fe900cd712d52")):
+    
+    #     image = face_recognition.load_image_file(os.path.join(os.getcwd(), "64c5783c513fe900cd712d52", filename))
+
+    #     face_locations = face_recognition.face_locations(image)
+
+    #     img_cv2 = cv2.imread(os.path.join(os.getcwd(), f"64c5783c513fe900cd712d52/{filename}"))
+
+    #     top, right, bottom, left = face_locations[0]
+
+    #     cropped_face = img_cv2[top:bottom, left:right]
+
+    #     grayscale_image = cv2.cvtColor(cropped_face, cv2.COLOR_BGR2GRAY)
+
+    #     scaled_cropped_grayscale_image = cv2.resize(grayscale_image, (40, 40))
+
+    #     x.append(scaled_cropped_grayscale_image)
+            
+    #     y.append(1)
+
+
+
+
+
+
+async def train_evaluate_update(lenght_of_user:int):
+
+
+    # model = tf.keras.models.Sequential([
+
+    #     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(40, 40, 1)),
+    #     tf.keras.layers.MaxPooling2D((2, 2)),
+        
+    #     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    #     tf.keras.layers.MaxPooling2D((2, 2)),
+        
+    #     tf.keras.layers.Flatten(),
+    #     tf.keras.layers.Dense(64, activation='relu'),
+    #     tf.keras.layers.Dense(2, activation='softmax')
+    # ])
+
+
+
+    # model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    # model.fit(x_train, y_train, epochs=30)
+
+    # test_loss, test_acc = model.evaluate(x_test, y_test)
+
+    # test_loss, test_acc
+
+    return 
+
+
+
 
 
 
